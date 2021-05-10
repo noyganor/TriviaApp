1b28dc551ae088301c6224e796eb4d907789d375
ronico612 <ronico612@gmail.com>
2021-05-10 15:27:09 +0300

    Add project files.

---
+++ TriviaXamarinApp/TriviaXamarinApp/ViewModels/GameViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TriviaXamarinApp.Models;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using TriviaXamarinApp.Services;
+using TriviaXamarinApp.Views;
+
+namespace TriviaXamarinApp.ViewModels
+{
+    class GameViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string question;
+        public string Question
+        {
+            get
+            {
+                return this.question;
+            }
+            set
+            {
+                if (this.question != value)
+                {
+                    this.question = value;
+                    OnPropertyChanged("Question");
+                }
+            }
+        }
+    }
+}
---
+++ TriviaXamarinApp/TriviaXamarinApp/ViewModels/PrivateQuestionsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TriviaXamarinApp.Models;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using TriviaXamarinApp.Services;
+
+namespace TriviaXamarinApp.ViewModels
+{
+    class PrivateQuestionsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //public string NickName { get; set; }
+        //public string Email { get; set; }
+        //public string Password { get; set; }
+
+        private string questionText;
+        public string QuestionText
+        {
+            get
+            {
+                return this.questionText;
+            }
+            set
+            {
+                if (this.questionText != value)
+                {
+                    this.questionText = value;
+                    OnPropertyChanged("QuestionText");
+                }
+            }
+        }
+
+        public Command AddButton => new Command(AddQuestion);
+        public async void AddQuestion()
+        {
+            TriviaWebAPIProxy proxy = TriviaWebAPIProxy.CreateProxy();
+            AmericanQuestion q = new AmericanQuestion
+            {
+                QText = this.questionText,
+                //CorrectAnswer = this.
+            };
+            bool succeeded = await proxy.PostNewQuestion(q);
+        }
+
+    }
+
+
+}
+
+
+
+    
+
